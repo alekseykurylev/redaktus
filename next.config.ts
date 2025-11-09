4a4c2373668e086abcbe7c16aab7d7c266c925e0
Aleksey Kurylev <alekseykurylev@yandex.ru>
2025-11-09 13:41:56 +0300

    fix icon

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   reactCompiler: true,
+  // images: {
+  //   loader: 'custom',
+  //   loaderFile: './loader.ts',
+  // },
 }
 
 export default nextConfig
