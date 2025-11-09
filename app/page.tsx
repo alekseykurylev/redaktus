336fa58faecfe31f4ef8f69a11e514ac3882d5a2
Aleksey Kurylev <alekseykurylev@yandex.ru>
2025-11-09 16:34:39 +0300

    font

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-5xl font-bold">Редактус</h1>
+      <p className="text-5xl">Whereas a common understanding of these rights and freedoms is</p>
       <Link href="/promo" className="text-blue-500">
         page
       </Link>
