d260cf46de730eb2fc5acf5705396a350d0357be
Aleksey Kurylev <alekseykurylev@yandex.ru>
2025-11-09 16:20:38 +0300

    style

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
+      <h1 className="text-5xl font-bold">Редактус</h1>
       <Link href="/promo" className="text-blue-500">
-        Promo
+        page
       </Link>
     </div>
   )
