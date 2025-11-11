492001bed9e14b31b2c4ad0d785622509ea7c6ad
Aleksey Kurylev <alekseykurylev@yandex.ru>
2025-11-11 21:42:28 +0300

    feat: app

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
-import Link from 'next/link'
-import Image from 'next/image'
+'use client'
+
+import { useEffect } from 'react'
+import { useDoc, useDocsActions, useDocActive } from '@/lib/store'
+import { AppShell } from '@/components'
 
 export default function Home() {
+  const { load } = useDocsActions()
+
+  const activeId = useDocActive()
+  const note = useDoc(activeId)
+
+  useEffect(() => {
+    load().catch()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (!note) return null
+
   return (
-    <div>
-      <h1 className="text-5xl font-bold">Редактус</h1>
-      <p className="text-5xl">Whereas a common understanding of these rights and freedoms is</p>
-      <Link href="/promo" className="text-blue-500">
-        page
-      </Link>
-    </div>
+    <AppShell.Root>
+      <AppShell.Sidebar>
+        <AppShell.Header>
+          <h1 className="text-2xl font-bold">Редактус</h1>
+        </AppShell.Header>
+        <AppShell.List>List</AppShell.List>
+        <AppShell.Footer />
+      </AppShell.Sidebar>
+      <AppShell.Doc>Doc</AppShell.Doc>
+    </AppShell.Root>
   )
 }
