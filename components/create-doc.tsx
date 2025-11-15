4c937a97f714602712f8df0e6f2c3f59fe801d0d
Aleksey Kurylev <alekseykurylev@yandex.ru>
2025-11-15 11:00:43 +0300

    feat: CreateDoc

--- components/create-doc.tsx
+++ components/create-doc.tsx
@@ -3,14 +3,14 @@ import { useDoc, useDocsActions, useDocActive } from '@/lib/store'
 
 export function CreateDoc(props: ComponentProps<'button'>) {
   const activeId = useDocActive()
-  const note = useDoc(activeId)
+  const doc = useDoc(activeId)
   const { createDoc } = useDocsActions()
 
   const handleClick = () => {
     createDoc().catch((e) => console.log(e))
   }
 
-  const isDisabled = note?.title?.length === 0
+  const isDisabled = doc?.title?.length === 0
 
   return (
     <button
